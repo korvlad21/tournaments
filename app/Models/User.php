0cdefcc58a57cc1fa8d3051162931a6c8ed52306
Vlad <vladek2000@inbox.ru>
2023-02-15 22:18:16 +0400

    roles and permissions

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\HasRolesAndPermissions;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -10,7 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, Notifiable, HasRolesAndPermissions;
 
     /**
      * The attributes that are mass assignable.
@@ -41,4 +42,94 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    /**
+     * @param mixed ...$roles
+     * @return bool
+     */
+    public function hasRole(... $roles ) {
+        foreach ($roles as $role) {
+            if ($this->roles->contains('slug', $role)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @param $permission
+     * @return bool
+     */
+    public function hasPermission($permission)
+    {
+        return (bool) $this->permissions->where('slug', $permission)->count();
+    }
+
+    /**
+     * @param $permission
+     * @return bool
+     */
+    public function hasPermissionTo($permission)
+    {
+        return $this->hasPermissionThroughRole($permission) || $this->hasPermission($permission->slug);
+    }
+
+    /**
+     * @param $permission
+     * @return bool
+     */
+    public function hasPermissionThroughRole($permission)
+    {
+        foreach ($permission->roles as $role){
+            if($this->roles->contains($role)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @param array $permissions
+     * @return mixed
+     */
+    public function getAllPermissions(array $permissions)
+    {
+        return Permission::whereIn('slug',$permissions)->get();
+    }
+    /**
+     * @param mixed ...$permissions
+     * @return $this
+     */
+    public function givePermissionsTo(... $permissions)
+    {
+        $permissions = $this->getAllPermissions($permissions);
+        if($permissions === null) {
+            return $this;
+        }
+        $this->permissions()->saveMany($permissions);
+        return $this;
+    }
+
+    /**
+     * @param mixed ...$permissions
+     * @return $this
+     */
+    public function deletePermissions(... $permissions )
+    {
+        $permissions = $this->getAllPermissions($permissions);
+        $this->permissions()->detach($permissions);
+        return $this;
+    }
+    /**
+     * @param mixed ...$permissions
+     * @return HasRolesAndPermissions
+     */
+    public function refreshPermissions(... $permissions )
+    {
+        $this->permissions()->detach();
+        return $this->givePermissionsTo($permissions);
+    }
+
+
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Permission;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = Role::where('slug','admin')->first();
+        $user1 = new User();
+        $user1->name = 'Jhon Deo';
+        $user1->email = 'jhon@deo.com';
+        $user1->password = bcrypt('secret');
+        $user1->save();
+        $user1->roles()->attach($admin);
+    }
+}
