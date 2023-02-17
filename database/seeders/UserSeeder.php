a4433efdc6c0312ec6f5acdae06dea36a22a986d
Vlad <vladek2000@inbox.ru>
2023-02-17 22:01:07 +0400

    get api

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,12 +16,17 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $admin = Role::where('slug','admin')->first();
+//        $admin = Role::where('slug','admin')->first();
+//        $user1 = new User();
+//        $user1->name = 'Jhon Deo';
+//        $user1->email = 'jhon@deo.com';
+//        $user1->password = bcrypt('secret');
+//        $user1->save();
+//        $user1->roles()->attach($admin);
         $user1 = new User();
-        $user1->name = 'Jhon Deo';
-        $user1->email = 'jhon@deo.com';
+        $user1->name = 'Alex Carrot';
+        $user1->email = 'alex@inbox.com';
         $user1->password = bcrypt('secret');
         $user1->save();
-        $user1->roles()->attach($admin);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::group(['middleware' => 'auth:sanctum'], function() {
+    Route::group(['middleware' => 'role:web-developer'], function() {
+        Route::get('/get', 'GetController');
+    });
+});
