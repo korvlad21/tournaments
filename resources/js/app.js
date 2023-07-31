8a0c4474fa159d04394e0a0820444fc909958995
Vladimir <pestov.web@yandex.ru>
2023-08-01 02:21:30 +1000

    stages

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,7 @@
-
-require('./bootstrap')
-import { createApp } from 'vue'
-import ExampleComponent from './components/ExampleComponent'
-import TestComponent from './components/TestComponent'
+require("./bootstrap");
+import { createApp } from "vue";
+import ExampleComponent from "./components/ExampleComponent";
+import TestComponent from "./components/TestComponent";
 import ProfileUpdate from "./components/profile/ProfileUpdate.vue";
 import TeamUpdate from "./components/team/TeamUpdate.vue";
 import TeamShow from "./components/team/TeamShow.vue";
@@ -19,36 +18,37 @@ import TournamentUpdate from "./components/tournament/TournamentUpdate.vue";
 import PlaceUpdate from "./components/place/PlaceUpdate.vue";
 import PlaceShow from "./components/place/PlaceShow.vue";
 import PlaceIndex from "./components/place/PlaceIndex.vue";
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import { plugin, defaultConfig } from "@formkit/vue";
 
 const vuetify = createVuetify({
+    plugin,
     components,
     directives,
-})
+});
 
+const app = createApp({});
 
-const app = createApp({})
+app.component("example-component", ExampleComponent);
+app.component("test-component", TestComponent);
+app.component("profile-show", ProfileShow);
+app.component("profile-update", ProfileUpdate);
+app.component("profile", Profile);
+app.component("team-update", TeamUpdate);
+app.component("team-show", TeamShow);
+app.component("team-index", TeamIndex);
+app.component("contractor-index", ContractorIndex);
+app.component("contractor-update", ContractorUpdate);
+app.component("contractor-show", ContractorShow);
+app.component("event-update", EventUpdate);
+app.component("event-show", EventShow);
+app.component("event-index", EventIndex);
+app.component("place-update", PlaceUpdate);
+app.component("place-show", PlaceShow);
+app.component("place-index", PlaceIndex);
+app.component("tournament-update", TournamentUpdate);
 
-app.component('example-component', ExampleComponent)
-app.component('test-component', TestComponent)
-app.component('profile-show', ProfileShow)
-app.component('profile-update', ProfileUpdate)
-app.component('profile', Profile)
-app.component('team-update', TeamUpdate)
-app.component('team-show', TeamShow)
-app.component('team-index', TeamIndex)
-app.component('contractor-index', ContractorIndex)
-app.component('contractor-update', ContractorUpdate)
-app.component('contractor-show', ContractorShow)
-app.component('event-update', EventUpdate)
-app.component('event-show', EventShow)
-app.component('event-index', EventIndex)
-app.component('place-update', PlaceUpdate)
-app.component('place-show', PlaceShow)
-app.component('place-index', PlaceIndex)
-app.component('tournament-update', TournamentUpdate)
-
-app.use(vuetify).mount('#app')
+app.use(vuetify).mount("#app");
---
+++ resources/js/components/shared/Stage.vue
@@ -0,0 +1,52 @@
+<script setup>
+const props = defineProps({
+    stageCount: Number,
+    discipline: String,
+    stagesData: Object,
+    stageName: Object,
+    discipline: Array,
+});
+</script>
+
+<template>
+    <div class="p-6">
+        <h2>Стадия: {{ stageCount }}</h2>
+        <div class="form-row">
+            <div class="form-group col-md-4">
+                <span>Название стадии</span>
+                <input
+                    v-model="stagesData[stageName[stageCount]].name"
+                    type="text"
+                    class="form-control"
+                />
+            </div>
+            <div class="form-group col-md-4">
+                <span>Количество групп</span>
+                <input
+                    v-model="stagesData[stageName[stageCount]].groupCount"
+                    type="number"
+                    class="form-control"
+                />
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-4">
+                <span>Количество игр</span>
+                <input
+                    v-model="stagesData[stageName[stageCount]].gamesCount"
+                    type="number"
+                    class="form-control"
+                />
+            </div>
+            <div class="form-group col-md-4">
+                <span>Количество команд для перехода</span>
+                <input
+                    v-model="stagesData[stageName[stageCount]].teamsCount"
+                    type="number"
+                    class="form-control"
+                />
+            </div>
+        </div>
+        <div class="form-row p-6"></div>
+    </div>
+</template>
