f2da1d91347a17b3a5aa6f299b07600d13d3c928
Vladimir <pestov.web@yandex.ru>
2023-08-09 20:08:42 +1000

    feat: modal w child

--- resources/js/components/event/EventUpdate.vue
+++ resources/js/components/event/EventUpdate.vue
@@ -2,11 +2,7 @@
     <div class="form-row p-6">
         <div class="form-group col-md-4">
             <span>Название</span>
-            <input
-                v-model="event.name"
-                type="text"
-                class="form-control"
-            />
+            <input v-model="event.name" type="text" class="form-control" />
         </div>
         <div class="form-group col-md-4">
             <span>Описание</span>
@@ -24,7 +20,7 @@
     <div class="form-row p-6">
         <div class="form-group col-md-4">
             <label class="block text-sm font-medium leading-6 text-gray-900"
-            >Картинка логотип банер</label
+                >Картинка логотип банер</label
             >
             <div class="mt-2 flex items-center">
                 <span
@@ -57,35 +53,37 @@
                 </label>
             </div>
         </div>
-            <div class="form-group col-md-4">
-                <span>Дата окончания</span>
-                <input v-model="event.end" type="date" class="form-control" />
-            </div>
-            <div class="form-group col-md-4">
-                <span>Контрагент</span>
-                <select v-model="event.contractor_id" class="form-control">
-                    <option
-                        v-for="contractorOption in contractorOptions"
-                        :value="contractorOption.id"
-                    >
-                        {{ contractorOption.name }}
-                    </option>
-                </select>
-            </div>
+        <div class="form-group col-md-4">
+            <span>Дата окончания</span>
+            <input v-model="event.end" type="date" class="form-control" />
         </div>
+        <div class="form-group col-md-4">
+            <span>Контрагент</span>
+            <select v-model="event.contractor_id" class="form-control">
+                <option
+                    v-for="contractorOption in contractorOptions"
+                    :value="contractorOption.id"
+                >
+                    {{ contractorOption.name }}
+                </option>
+            </select>
+        </div>
+    </div>
     <div class="form-row p-6">
         <div class="form-group col-md-4">
-            <button v-if="undefined === this.id"
+            <button
+                v-if="undefined === this.id"
                 type="submit"
                 class="btn btn-primary btn15"
                 @click="createEvent"
             >
                 Создать эвент
             </button>
-            <button v-else
-                    type="submit"
-                    class="btn btn-primary btn15"
-                    @click="updateEvent"
+            <button
+                v-else
+                type="submit"
+                class="btn btn-primary btn15"
+                @click="updateEvent"
             >
                 Обновить эвент
             </button>
@@ -101,9 +99,10 @@
 </template>
 
 <script>
-
 import { ref } from "vue";
 import ModalDialog from "../shared/ModalDialog.vue";
+import Modal from "../shared/ModalWithChild.vue";
+
 const isOpen = ref(false);
 const isSharedOpen = ref(false);
 
@@ -114,6 +113,7 @@ export default {
     },
     data() {
         return {
+            isModalOpen: false,
             event: {
                 name: "",
                 description: "",
@@ -169,7 +169,7 @@ export default {
             formData.append("contractor_id", this.event.contractor_id);
             formData.append("logo", this.logo);
             axios
-                .post("/api/event/update/" +this.id, formData, {
+                .post("/api/event/update/" + this.id, formData, {
                     headers: {
                         "Content-Type": "multipart/form-data",
                     },
@@ -188,10 +188,11 @@ export default {
             if (undefined === this.id) {
                 return;
             }
-            axios.post('/api/event/get_info/', {
-                id: this.id
-            })
-                .then(({data}) => {
+            axios
+                .post("/api/event/get_info/", {
+                    id: this.id,
+                })
+                .then(({ data }) => {
                     this.event.name = data.name;
                     this.event.description = data.description;
                     this.event.start = data.start;
@@ -201,22 +202,20 @@ export default {
                 .catch((error) => {
                     console.error(error);
                 })
-                .finally(() => {
-                });
+                .finally(() => {});
         },
 
         getContractorOptions() {
-            axios.post('/api/contractor/get_options/')
-                .then(({data}) => {
-                    console.log(data.contractorOptions)
-                    this.contractorOptions = data.contractorOptions
+            axios
+                .post("/api/contractor/get_options/")
+                .then(({ data }) => {
+                    console.log(data.contractorOptions);
+                    this.contractorOptions = data.contractorOptions;
                 })
                 .catch((error) => {
                     console.error(error);
                 })
-                .finally(() => {
-                });
-
+                .finally(() => {});
         },
         setModalIsOpen(value) {
             isOpen.value = value;
---
+++ resources/js/components/shared/ModalWithChild.vue
@@ -0,0 +1,52 @@
+<template>
+    <transition name="modal-fade">
+        <div v-if="isOpen" class="modal-overlay">
+            <div class="modal-container">
+                <div class="modal-content">
+                    <slot></slot>
+                </div>
+                <button class="modal-close-button" @click="closeModal">
+                    Закрыть
+                </button>
+            </div>
+        </div>
+    </transition>
+</template>
+
+<script>
+export default {
+    props: {
+        isOpen: Boolean,
+    },
+    methods: {
+        closeModal() {
+            this.$emit("update:isOpen", false);
+        },
+    },
+};
+</script>
+
+<style scoped>
+.modal-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.modal-container {
+    background-color: white;
+    padding: 20px;
+    border-radius: 8px;
+    box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
+}
+
+.modal-close-button {
+    margin-top: 10px;
+}
+</style>
