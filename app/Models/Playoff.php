0e4cd399aa59e0ec3a201f35cf8cc750a27d1bc4
Vlad <vladek2000@inbox.ru>
2023-11-14 22:10:26 +0400

    finish

--- app/Models/Playoff.php
+++ app/Models/Playoff.php
@@ -4,19 +4,35 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Playoff extends Model
 {
     protected $fillable = [
-        'team_win_play_off_id',
-        'team_lost_play_off_id',
+        'team1_win_play_off_id',
+        'team2_win_play_off_id',
+        'team1_exist',
+        'team2_exist',
         'level',
         'number',
-        'game_id'
+        'game_id',
+        'group_id',
     ];
 
-    public function game(): \Illuminate\Database\Eloquent\Relations\HasOne
+    /**
+     * @return BelongsTo
+     */
+    public function group(): BelongsTo
     {
-        return $this->hasOne(Game::class);
+        return $this->belongsTo(Group::class);
+    }
+
+    public function team1(): \Illuminate\Database\Eloquent\Relations\HasOne
+    {
+        return $this->hasOne(Team::class, 'id', 'team1_win_play_off_id');
+    }
+    public function team2(): \Illuminate\Database\Eloquent\Relations\HasOne
+    {
+        return $this->hasOne(Team::class, 'id', 'team2_win_play_off_id');
     }
 }
